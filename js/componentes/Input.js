19d2cb0c1741670d3c048036327b5988dfe287e7
paulocardosob <paulocardosob@gmail.com>
2017-11-10 16:29:36 -0300

    Input estilizado adicionado

---
+++ js/componentes/Input.js
@@ -0,0 +1,16 @@
+//Componente de texto que podera ser reusado em diversas telas
+
+function Input(x, y, texto = ''){
+  //Atributos que serao usados para desenhar o campo de texto
+  this.x = x;
+  this.y = y;
+  this.texto = texto;
+  this.campo;
+  this.draw = function(){
+    campo = createInput();
+    campo.position(x,y);
+    campo.attribute("type", "text");
+    campo.attribute("placeholder", texto);
+  }
+
+}
