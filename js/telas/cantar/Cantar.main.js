7ac875ab64e2aaf788f262803b1d553eaed85ea0
Mateus Santos <33528161+uaumateus@users.noreply.github.com>
2017-12-06 16:37:16 -0300

    Telas de tutorial e novo pulsar

--- js/telas/cantar/Cantar.main.js
+++ js/telas/cantar/Cantar.main.js
@@ -4,9 +4,9 @@ function Cantar(){
   var continuarButton = new Button(498, 587, btnGradient, 'CONTINUAR');
   var timbre = loadImage('assets/cantar/timbre.png');
   var comment = loadImage('assets/cantar/comment.png');
-
-  var myText = "Para esse exercício, você terá que cantar a nota pedida. Para lhe ajudar, escute-a clicando no botão       .Você também terá que manter o som de sua voz por pelo menos 2 segundos. ";
-
+  var explicacao = loadImage('assets/cantar/explicacao.png');
+  var myText = "Para esse exercício, você terá que cantar a nota pedida. Para lhe ajudar, escute a nota clicando no botão abaixo.";
+  var myText2 = "Quando você atingir a frequência da nota, você terá que manter o som de sua voz por pelo menos 2 segundos.";
   this.draw = function(){
     clear();
     background(bgNoise);
@@ -14,14 +14,15 @@ function Cantar(){
     backButton.draw();
     // image(timbre, 160, 143);
     // image(comment, 160, 233);
-    image(iconSound, width/2+30, height/3+40);
+    //image(iconSound, width/2+30, height/3+40);
     textFont(boldFont);
     textSize(40);
-    text("TIMBRE", width/7, height/4);
+    text("TIMBRE", width/7, 150);
     textFont(regularFont);
     textSize(30);
-    drawText(myText);
-
+    drawText(myText, width/7, 180);
+    image(explicacao, 600, 290);
+    drawText(myText2, width/7, 410);
     continuarButton.draw();
 
     checkPress();
@@ -39,7 +40,7 @@ function Cantar(){
 
   };
 
-  var drawText = function(myText) {
-    text(myText, width/7, height/3, 915, 150);
+  var drawText = function(myText, x, y) {
+    text(myText, x, y, 915, 150);
   };
 }
--- js/telas/intervalos/Intervalos.main.js
+++ js/telas/intervalos/Intervalos.main.js
@@ -3,7 +3,8 @@ function Intervalos(){
   var backButton = new Button(50, 50, btnBack);
   var continuarButton = new Button(width/2-142, height-height/6, btnGradient, "CONTINUAR");
   var myText = "Nesse exercício você terá que colocar a nota na ordem certa da escala. Arraste a nota do botão         e coloque dentro dos círculos brancos."
-
+  var explicacao = loadImage('assets/intervalos/explicacao.png');
+  var myText = "Para esse exercício você terá que colocar as notas na ordem certa do intervalo. Para isso, arraste os botões das notas para dentro dos círculos.";
   this.draw = function(){
 
     clear();
@@ -14,11 +15,12 @@ function Intervalos(){
 
     textSize(40);
     textFont(boldFont);
-    text("INTERVALOS", width/7, height/4);
+    text("INTERVALOS", width/7, 150);
     textFont(regularFont);
     textSize(30);
-    image(iconSound, width/2-5, height/3+40);
-    drawText(myText);
+    //image(iconSound, width/2-5, height/3+40);
+    drawText(myText, width/7, 180);
+    image(explicacao, 283, 312);
 
     continuarButton.draw();
 
@@ -38,8 +40,8 @@ function Intervalos(){
 
   }
 
-  var drawText = function(myText) {
-    text(myText, width/7, height/3, 915, 150);
+  var drawText = function(myText, x, y) {
+    text(myText, x, y, 915, 150);
   };
 
 };
