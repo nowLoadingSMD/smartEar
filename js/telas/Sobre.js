a946e0d9020d9feb28058c9c3bad30e4103bc7ee
Rebecca Dantas <rbxdan@gmail.com>
2017-12-07 01:26:28 -0300

    Atualizando tela Sobre

--- js/telas/Sobre.js
+++ js/telas/Sobre.js
@@ -1,18 +1,14 @@
 function Sobre(){
 
 	var logo = loadImage('assets/menu/logo_menu.png');
-<<<<<<< HEAD
-	var jogosButton = new Button(770, 19, btnJogos);
-	var perfilButton = new Button(921, 19, btnPerfil);
-	var sobreButton = new Button(1072, 19, btnSobrePress);
-=======
-	var blur = loadImage('assets/blur.png');
+	var nowloading = loadImage('assets/sobre/nowloadinglogo.png');
+	var ufc = loadImage('assets/sobre/ufclogo.png');
+	var smd = loadImage('assets/sobre/smdlogo.png');
 
 	var jogosButton = new Button(613, 19, btnJogos);
   var licoesButton = new Button(764, 19, btnLicoesMenu);
   var perfilButton = new Button(921, 19, btnPerfil);
   var sobreButton = new Button(1072, 19, btnSobrePress);
->>>>>>> 803372602c3f700ad82acf9190de905f396c0aa4
 
 	this.draw = function(){
     clear();
@@ -28,15 +24,23 @@ function Sobre(){
     perfilButton.draw();
     sobreButton.draw();
 
+		ufc.resize(62, 102);
+		smd.resize(117, 90);
+		nowloading.resize(492, 127);
+
     image(logo, 82, 14);
 		textFont(boldFont);
-		textSize(42);
-		text("EQUIPE", 156, 200);
-		text("ORIENTADORES", 156, 440);
-		textSize(30);
+		textSize(32);
+		text("Smart Ear é um projeto desenvolvido pela equipe", 156, 200);
+		image(nowloading, 156, 244);
+		text("sob a orientação dos professores", 156, 445);
 		textFont(regularFont);
-		text("Douglas Silva, Mateus Pereira, Paulo José, Rebecca Dantas, Thays Lunes", 156, 230, 915, 150);
-		text("José Aires, Raquel Freire, Glaudiney Mendonça, Georgia Cruz, Ricardo Brauner, Paula Marques", 156, 470, 915, 150);
+		// text("Douglas Silva, Mateus Pereira, Paulo José, Rebecca Dantas, Thays Lunes", 156, 230, 915, 150);
+		text("José Aires, Raquel Freire, Glaudiney Mendonça, Georgia Cruz, Ricardo Brauner e Paula Marques.", 156, 475, 915, 150);
+		image(ufc, 1019, 573);
+		image(smd, 1103, 576);
+
+
 
     checkPress();
 
